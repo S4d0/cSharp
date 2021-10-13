a18de0b0244840a68396af0e53a2b2bb1c990cfc
S4d0 <88021410+S4d0@users.noreply.github.com>
2021-10-14 01:35:15 +0300

    Add files via upload

---
+++ 2-addingValuesInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// create 10 element array and adding user entered values to the array.
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating array with 10 elements
+            int[] Arr = new int[10];
+            int a;
+            // inserting array elements
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Please insert the {0} element for the array: ",i+1);
+                a =Convert.ToInt32(Console.ReadLine());
+                Arr[i] = a;
+            }
+
+            // squaring array elements and finding the average.
+            int sum = 0;
+            double avrg(int elemntNumber)
+            {
+                for (int i = 0; i < elemntNumber; i++)
+                {
+                    sum += Arr[i] * Arr[i];
+                }
+                return sum / elemntNumber; 
+            }
+
+            Console.WriteLine(avrg(10));
+        }
+    }
+}
---
+++ 3-enumPractise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enum
+{
+    class enumPractice
+    {
+        enum Workers: byte
+        {
+            manager,
+            teacher,
+            officer,
+            attendant
+        }
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[] { 12000, 8000, 6000, 3700};
+
+            Console.Write("Manager = 0\nTeacher =1\nOfficer = 2\nAttendant = 3\n");
+            Console.Write("Please, insert a value = ");
+            Workers Value = (Workers)Convert.ToByte(Console.ReadLine());
+            switch (Value)
+            {
+                case Workers.manager :
+                    Console.WriteLine("Manager Salary = " + Arr[0]);
+                    break;
+                case Workers.teacher:
+                    Console.WriteLine("Teacher Salary = " + Arr[1]);
+                    break;
+                case Workers.officer:
+                    Console.WriteLine("Officer Salary = " + Arr[2]);
+                    break;
+                case Workers.attendant:
+                    Console.WriteLine("Attendant Salary = " + Arr[3]);
+                    break;
+            }
+        }
+    }
+}
